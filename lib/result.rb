1f5303ef5bc1f323be0c7f66910155fea7361fe0
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-08-08 15:11:01 -0300

    Making it work with singleton method

--- lib/result.rb
+++ lib/result.rb
@@ -5,14 +5,16 @@ require 'dry/monads/all'
 module MonadicExceptions
   # Result wrap functions responsible to return Result monads
   class Result
-    include Dry::Monads[:result]
-
     # wraps a function and act as a interception
     # returning to you result monads instead.
     # @param callback Function
-    # @return [Failure({error: Symbol, where: String}), Success(data)]
-    def from_exception(callback)
-      puts callback
+    # @return [Failure({error: Symbol, where: String, orig_exception: Exception}), Success(data)]
+    def self.from_exception(callback)
+      result = method(callback).call
+
+      Dry::Monads::Result::Success.new(result)
+    rescue => e
+      Dry::Monads::Result::Failure.new({ error: e.to_s.downcase.to_sym, where: callback, orig_exception: e })
     end
   end
 end
